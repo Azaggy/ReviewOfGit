57ad9d39970dce73e26a5be086b29781d70128bb
Administrator <azaggy1@outlook.com>
2021-04-24 20:27:59 -0500

    Initial Commit

---
+++ src/main/java/org/launchcode/ReviewOfGit/ReviewOfGitApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.ReviewOfGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewOfGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewOfGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/ReviewOfGit/ReviewOfGitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.ReviewOfGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewOfGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
